229ed12fd2a0ee52eed3c1a7ece3ff98bc908ac8
Student <student@BCCSs-MacBook-Pro.local>
2024-01-23 10:51:03 -0600

    Complete Excercise

---
+++ script.js
@@ -0,0 +1,19 @@
+ console.log(6);
+
+//  numbers
+// strings 
+// boolean 
+// null
+// undefined
+// +,-,*,/,**,%
+console.log(5%4);
+
+console.log('I\'M MARY POPPINS Y\'ALL');
+
+console.log("Hawk" + "eye");
+
+console.log("Jason".length + "Willis".length);
+
+console.log("Avengers: Endgame"[16]);
+
+
